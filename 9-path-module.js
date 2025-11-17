e711e439ea1f910c7a643943d7623c8d8365dbe7
Naveen Kumar <kumar20maverick@gmail.com>
2025-11-17 08:51:42 +0530

    node tutorial initial commit

---
+++ 9-path-module.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+// path separator
+console.log(path.sep);
+
+// file path
+const filePath = path.join('content', 'subfolder', 'test.txt');
+console.log(filePath);
+
+// base path
+const base = path.basename(filePath);
+console.log(base);
+
+// absolute path
+const absolute = path.resolve(__dirname, 'content', 'subfolder', 'test.txt');
+console.log(absolute);
